109ecf48cac3adfec6e3a0c29b7ad41ca5ccc8e7
ignatha <prabowoid.2@gmail.com>
2025-05-10 14:07:54 +0700

    update ajax

--- resources/views/master/app.blade.php
+++ resources/views/master/app.blade.php
@@ -3,6 +3,7 @@
 <head>
 <head>
     <meta charset="UTF-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="/assets/css/app.css" type="text/css">
